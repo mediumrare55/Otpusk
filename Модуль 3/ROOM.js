9fc0ee1142b12596690484df509183a08dce3e5e
mediumrare55 <95922671+mediumrare55@users.noreply.github.com>
2025-08-17 15:10:09 +0600

    Add files via upload

---
+++ Модуль 3/ROOM.js
@@ -0,0 +1,8 @@
+alert("Если Вы не являетесь Юрием Юрьевичем Чащиным, покиньте страницу          https://static.tildacdn.com/tild6431-3436-4636-a664-336134393863/photo.png");
+document.addEventListener("click",overflow);
+function overflow(){
+    bb=document.getElementById("PASS").value;
+    if (bb=="qwerty799"){
+    document.getElementById("vid").style.opacity=100;
+    } 
+}
